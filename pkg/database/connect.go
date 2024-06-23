932e03e3a482a168d26d3e419a0b866a6428b084
shervin_yazdan <shervin.yazdan75@gmail.com>
2024-06-23 12:07:44 +0330

    define function Connect for connecting to mysql

--- pkg/database/connect.go
+++ pkg/database/connect.go
@@ -1,1 +1,25 @@
 package database
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"phonebook/pkg/config"
+)
+
+func Connect() {
+	configuration := config.Get()
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		configuration.DB.Username,
+		configuration.DB.Password,
+		configuration.DB.Host,
+		configuration.DB.Port,
+		configuration.DB.Name,
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	DB = db
+}
