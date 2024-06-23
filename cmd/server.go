75648bf115f900a0f0c868df9bb1ca07f935f72e
shervin_yazdan <shervin.yazdan75@gmail.com>
2024-06-23 13:38:32 +0330

    define server on cli

--- cmd/server.go
+++ cmd/server.go
@@ -1,5 +1,21 @@
 package cmd
 
-func Server() {
-	//TODO:add serve the host
+import (
+	"github.com/spf13/cobra"
+	"log"
+	"phonebook/pkg/bootstrap"
+)
+
+func Execute() {
+	root := &cobra.Command{
+		Use:     "Phonebook",
+		Short:   "serve the Phone Book",
+		Version: "0.1",
+	}
+
+	bootstrap.Server()
+
+	if err := root.Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
