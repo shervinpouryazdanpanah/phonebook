0a0a23b4f79a6619b6d05237640b260e6bb13397
shervin_yazdan <shervin.yazdan75@gmail.com>
2024-06-25 17:58:57 +0330

    create migration file

---
+++ internal/database/migration.go
@@ -0,0 +1,17 @@
+package database
+
+import (
+	"fmt"
+	phoneModel "phonebook/internal/modules/phone/models"
+	userModel "phonebook/internal/modules/user/models"
+	"phonebook/pkg/database"
+)
+
+func Migration() {
+	db := database.Connection()
+	err := db.AutoMigrate(&userModel.User{}, &phoneModel.Phone{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("migration success")
+}
