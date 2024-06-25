8c1ca43fda0e48b2d73263b9710c55ca59a4d296
shervin_yazdan <shervin.yazdan75@gmail.com>
2024-06-25 17:59:34 +0330

    create user model

---
+++ internal/modules/user/models/user_model.go
@@ -0,0 +1,10 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name     string `gorm:"varchar(255)" json:"name"`
+	Username string `gorm:"varchar(255)" json:"username"`
+	Password string `gorm:"->:false;<-:create" json:"password"`
+}
