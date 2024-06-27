fc15206c84904dcdd8b7263ad7715a02ff2e953d
s.yazdan <s.yazdan@sikabiz.com>
2024-06-28 00:26:12 +0330

    update migration cli

--- cmd/migration.go
+++ cmd/migration.go
@@ -2,7 +2,7 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
-	"phonebook/internal/database"
+	"phonebook/internal/database/migration"
 )
 
 var migrationCmd = &cobra.Command{
@@ -10,7 +10,7 @@ var migrationCmd = &cobra.Command{
 	Short: "Table migration",
 	Long:  "All tables will be migrated on username, password, host and port defined in config.yml ",
 	Run: func(cmd *cobra.Command, args []string) {
-		database.Migration()
+		migration.Migration()
 	},
 }
 
