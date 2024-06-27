96b3dd6a5e1b96127809b43ef3a8ba5e41c93d2d
s.yazdan <s.yazdan@sikabiz.com>
2024-06-28 00:27:00 +0330

    update phone repository

--- internal/modules/phone/repositories/phone_repository.go
+++ internal/modules/phone/repositories/phone_repository.go
@@ -23,13 +23,20 @@ func (phoneRepository *PhoneRepository) List(limit int, userId int) []models.Pho
 	return phoneList
 }
 
-func (phoneRepository *PhoneRepository) find(id int) *models.Phone {
-	var phone models.Phone
-	phoneRepository.DB.Joins("User").First(&phone, id)
-	return &phone
+func (phoneRepository *PhoneRepository) All(userId int) []models.Phone {
+	var phoneList []models.Phone
+	phoneRepository.DB.Joins("User").Order("created_at desc").
+		Find(&phoneList, "user_id = ?", userId)
+	return phoneList
 }
 
-func (phoneRepository *PhoneRepository) Create(phone *models.Phone) models.Phone {
+func (phoneRepository *PhoneRepository) Find(id int) models.Phone {
+	var phone models.Phone
+	phoneRepository.DB.Joins("User").First(&phone, id)
+	return phone
+}
+
+func (phoneRepository *PhoneRepository) Create(phone models.Phone) models.Phone {
 	var newPhone models.Phone
 	result := phoneRepository.DB.Create(phone)
 	if result.Error != nil {
--- internal/modules/phone/repositories/phone_repository_interface.go
+++ internal/modules/phone/repositories/phone_repository_interface.go
@@ -4,6 +4,7 @@ import "phonebook/internal/modules/phone/models"
 
 type PhoneRepositoryInterface interface {
 	List(limit int, userId int) []models.Phone
-	find(id int) models.Phone
-	create(phone models.Phone) models.Phone
+	All(userId int) []models.Phone
+	Find(id int) models.Phone
+	Create(phone models.Phone) models.Phone
 }
