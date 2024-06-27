4a2aa2c1d8c7522065e774553a2e1323bcaa1889
s.yazdan <s.yazdan@sikabiz.com>
2024-06-28 00:25:42 +0330

    create response file for phone module

---
+++ internal/modules/phone/responses/phone_response.go
@@ -0,0 +1,30 @@
+package responses
+
+import (
+	phoneModel "phonebook/internal/modules/phone/models"
+	UserResponse "phonebook/internal/modules/user/responses"
+)
+
+type Phone struct {
+	Number string
+	User   UserResponse.User
+}
+
+type Phones struct {
+	Data []Phone `json:"data"`
+}
+
+func ToPhone(phone phoneModel.Phone) Phone {
+	return Phone{
+		Number: phone.Number,
+		User:   UserResponse.ToUser(phone.User),
+	}
+}
+
+func ToPhones(phones []phoneModel.Phone) Phones {
+	var phonesData Phones
+	for _, phone := range phones {
+		phonesData.Data = append(phonesData.Data, ToPhone(phone))
+	}
+	return phonesData
+}
