03529d8349eb2c67b1195e718566675bee9e6c11
s.yazdan <s.yazdan@sikabiz.com>
2024-06-28 21:33:30 +0330

    set unique for username

--- internal/modules/user/models/user_model.go
+++ internal/modules/user/models/user_model.go
@@ -5,6 +5,6 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	Name     string `gorm:"varchar(255)" json:"name"`
-	Username string `gorm:"varchar(255)" json:"username"`
+	Username string `gorm:"unique;varchar(255)" json:"username"`
 	Password string `gorm:"->:false;<-:create" json:"password"`
 }
